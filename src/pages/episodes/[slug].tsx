899ba53952b3695310b8a1eabbae5977272d5ebe
Erick Malta <erickmalta100@gmail.com>
2021-04-23 00:33:54 -0300

    feat(episodes): add ISR to episode

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
@@ -61,8 +62,22 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
+
+  const paths = data.map(episode => ({
+    params: {
+      slug: episode.id,
+    }
+  }));
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking',
   }
 }
